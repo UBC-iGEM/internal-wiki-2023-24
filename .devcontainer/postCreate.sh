dafd84a9cc081958a11c81b2007fe237ea5abc17
Lucy H <55033656+lhao03@users.noreply.github.com>
2024-03-11 09:55:55 -0700

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,8 +2,9 @@
 
 sudo apt update
 sudo apt -y install graphviz
-sudo apt -y install pandoc
+sudo apt-get install pandoc
 
 cargo install mdbook-linkcheck
 cargo install mdbook-toc
 cargo install mdbook-graphviz
+cargo install mdbook-bibfile-referencing
