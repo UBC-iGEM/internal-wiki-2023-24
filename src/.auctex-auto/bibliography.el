817da84cdb7ffba5e4638a0f38f65da955aa378c
Lucy Hao <hao.lucyy@gmail.com>
2024-03-11 10:54:24 -0700

    add sources for encoding page

--- src/.auctex-auto/bibliography.el
+++ src/.auctex-auto/bibliography.el
@@ -3,6 +3,17 @@
  (lambda ()
    (LaTeX-add-bibitems
     "clines1974evidence"
-    "test"))
+    "test"
+    "Wu_Lee_Wu_Shiue_2004"
+    "Bornholt_Lopez_Carmean_Ceze_Seelig_Strauss_2016"
+    "Sharma_Lim_Lin_Pote_Jevdjic_2023"
+    "addgene2019"
+    "primerpcr"
+    "aachen2022"
+    "eisenstein2020"
+    "rbarcklay2020"
+    "wikipediautf"
+    "redundancy2020"
+    "howclosedna"))
  '(or :bibtex :latex))
 
